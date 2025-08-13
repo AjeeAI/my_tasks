960efbad7bfbd26dfbd8cf14c1ab6e9b7de52d82
AjeeAI <ajeeaidev@gmail.com>
2025-08-13 22:34:40 +0100

    Version for submission

--- week_2/task_2.py
+++ week_2/task_2.py
@@ -12,11 +12,13 @@
 Done according to guidelines
 
 """
-dishes = (input("Input your favorite dish: "),
-          input("Input your favorite dish: "),
-          input("Input your favorite dish: ")
-          )
-print(", ".join(dishes))
+dishes = (
+          input("Input your favorite dish: ").title(),
+          input("Input your favorite dish: ").title(),
+          input("Input your favorite dish: ").title()
+          ) # This is basically like a list of inputs.
+print(", ".join(dishes)) # This prints the dishes in a single line and separates them using commas.
+print("\n".join(dishes)) # This prints the dishes in separate lines.
 
 
 # # Task 2
@@ -30,18 +32,18 @@ print(", ".join(dishes))
 # friends = tuple(friends_names)
 # print(friends[::-1])
 
+
 """
 Task 2 done according to guideline
 """
-
 friends = (
-    input("Input your bestfriend's name: "),
-    input("Input your another bestfriend's name: "),
-    input("Input your another bestfriend's name: "),
-    input("Input your another bestfriend's name: "),
-    input("Input your another bestfriend's name: ")
+            input("Input your bestfriend's name: ").title(),
+            input("Input your another bestfriend's name: ").title(),
+            input("Input your another bestfriend's name: ").title(),
+            input("Input your another bestfriend's name: ").title(),
+            input("Input your another bestfriend's name: ").title()
 )
-print(friends[::-1])
+print(friends[::-1]) # This prints the bestfriends entry in reverse order.
 
 # # Task 3
 
@@ -63,40 +65,52 @@ print(friends[::-1])
 # print(len(states))
 
 
+
 """
 Task 3 done as per the guidelines given.
 """
-
-states = (input("Input the name of Nigerian state: "),
-          input("Input the name of another Nigerian state: "),
-          input("Input the name of another Nigerian state: "),
-          input("Input the name of another Nigerian state: "),
-          input("Input the name of another Nigerian state: "),
+states = (
+          input("Input the name of Nigerian state: ").title(),
+          input("Input the name of another Nigerian state: ").title(),
+          input("Input the name of another Nigerian state: ").title(),
+          input("Input the name of another Nigerian state: ").title(),
+          input("Input the name of another Nigerian state: ").title(),
           )
-print(f"First: {states[0]}\nLast: {states[-1]}")
-# Task 4
 
-profile = (input("First name: "), input("Age:"), input("Favorite_color: "), input("Home town: "), )
-first_name, age, fav_color, home_town = profile
+
+print(f"First: {states[0]}\nLast: {states[-1]}") # This prints the first and last state in the tuple.
+print("Lagos" in states) # This checks if "Lagos" is in the states tuple.
+print(f"The list of states contains {len(states)} states.") # This prints the length of the tuple, which is the number of states in it.
+
+
+# Task 4
+profile = (
+           input("First name: ").title(), 
+           input("Age: "), 
+           input("Favorite_color: ").title(), 
+           input("Home town: ").title(), 
+           )
+
+first_name, age, fav_color, home_town = profile # This unpacks the profile tuple into individual variables.
 print(f"User's first name is: {first_name}\nUser's age is: {age}\nFavorite color: {fav_color}\nHome town: {home_town}")
 
 
 # Task 5
-shopping_list_tuple = ( input("Input first item: "),
-    input("Input second item: "),
-    input("Input third item: ")
-    
-    )
+shopping_list = ( 
+                input("Input first item: ").title(),
+                input("Input second item: ").title(),
+                input("Input third item: ").title()
+                     )
 
-shopping_list = list(shopping_list_tuple)
+shopping_list = list(shopping_list) # This converts the tuple to a list so that we can extend it with more items.
 extra_items = [
-    input("Input first extra item: "),
-    input("Input second extra item: ")
-]
-shopping_list.extend(extra_items)
+            input("Input first extra item: ").title(),
+            input("Input second extra item: ").title()
+                ]
+shopping_list.extend(extra_items) # This extends the shopping list with extra items. Extend was used instead of append so as to avoid nested "lists".
 # print(shopping_list)
-shopping_list = tuple(shopping_list)
-print("|".join(shopping_list))
+shopping_list = tuple(shopping_list) # This converts the list back to a tuple.
+print("|".join(shopping_list)) # This prints the shopping list items separated by "|".
 
 
 # Task 6
@@ -104,14 +118,14 @@ days_of_the_week = ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Fri
 months = ("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
 
 prompt = [
-    input("Student's name: "),
-    input("Gender: "),
-    input("Course track: "),
-    input("Current month's number: "),
-    input("Current day number(For example: 1 for Sunday): ")
-]
+        input("Student's name: ").title(),
+        input("Gender: ").title(),
+        input("Course track: ").title(),
+        input("Current month's number(1-12): "),
+        input("Current day number(1-7)(For example: 1 for Sunday): ")
+        ]
 
-name, gender, track, month, day = prompt
-month = int(month) - 1
-day = int(day) - 1
-print(f"Student name: {name}\nGender: {gender}\nCourse track: {track}\nCurrent month: {months[month]}\nCurrent day: {days_of_the_week[day]}")
+name, gender, track, month, day = prompt # This unpacks the prompt tuple into individual variables.
+month = int(month) - 1 # Convert month to zero-based index
+day = int(day) - 1 # Convert day to zero-based index
+print(f"Student name: {name}\nGender: {gender}\nCourse track: {track}\nCurrent month: {months[month]}\nCurrent day: {days_of_the_week[day]}") # This prints the student's information.
